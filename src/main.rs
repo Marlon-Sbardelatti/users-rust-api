d04d85cd59e052379850f38e6e1fe92ea754d0b1
Marlon-Sbardelatti <hetzwga@gmail.com>
2024-05-09 17:47:02 -0300

    auth done

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
+// use rocket::http::Method;
+// use rocket_cors::{AllowedHeaders, AllowedMethods, AllowedOrigins};
+// use crate::models::Link;
+// use crate::models::NewLink;
+// use controllers::LinkController;
 use diesel::result::Error::NotFound;
-use std::error::Error;
-
-use rocket::http::Method;
 use rocket::{get, routes};
 use rocket::{
     response::status::Custom,
     serde::json::{json, Json, Value},
 };
-use rocket_cors::{AllowedHeaders, AllowedMethods, AllowedOrigins};
+use std::error::Error;
 mod auth;
 mod controllers;
 mod models;
 mod schema;
-use crate::models::Link;
-use crate::models::NewLink;
 use crate::models::NewUser;
 use crate::models::User;
 use auth::BasicAuth;
-use controllers::LinkController;
 use controllers::UserController;
 use controllers::Verifier;
 use rocket::http::Status;
 use rocket_sync_db_pools::database;
+
 #[macro_use]
 extern crate rocket;
 
@@ -32,20 +32,42 @@ struct DbConn(diesel::SqliteConnection);
 //USERS
 
 #[get("/users")]
-async fn all_users(db: DbConn) -> Result<Value, Custom<Value>> {
-    db.run(|c| match UserController::find_many(c) {
-        Ok(users) => Ok(json!(users)),
-        Err(e) => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-    })
+async fn all_users(auth: BasicAuth, db: DbConn) -> Result<Value, Custom<Value>> {
+    db.run(
+        move |c| match Verifier::verify(c, &auth.email, &auth.password) {
+            Ok(user) => {
+                println!("{:?}", user);
+                match UserController::find_many(c) {
+                    Ok(users) => Ok(json!(users)),
+                    Err(e) => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+                }
+            }
+            Err(e) => match e {
+                NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
+                _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+            },
+        },
+    )
     .await
 }
 
 #[get("/users/<id>")]
-async fn user(db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
-    db.run(move |c| match UserController::find_one(c, id) {
-        Ok(users) => Ok(json!(users)),
-        Err(e) => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-    })
+async fn user(auth: BasicAuth, db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
+    db.run(
+        move |c| match Verifier::verify(c, &auth.email, &auth.password) {
+            Ok(_user) => match UserController::find_one(c, id) {
+                Ok(user) => Ok(json!(user)),
+                Err(e) => match e {
+                    NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
+                    _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+                },
+            },
+            Err(e) => match e {
+                NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
+                _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+            },
+        },
+    )
     .await
 }
 
@@ -59,123 +81,36 @@ async fn create_user(db: DbConn, new_user: Json<NewUser>) -> Result<Value, Custo
 }
 
 #[put("/users/<id>", format = "json", data = "<user>")]
-async fn update_user(db: DbConn, user: Json<User>, id: i32) -> Result<Value, Custom<Value>> {
+async fn update_user(
+    auth: BasicAuth,
+    db: DbConn,
+    user: Json<User>,
+    id: i32,
+) -> Result<Value, Custom<Value>> {
     db.run(
-        move |c| match UserController::update(c, user.into_inner(), id) {
-            Ok(users) => Ok(json!(users)),
-            Err(e) => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+        move |c| match Verifier::verify(c, &auth.email, &auth.password) {
+            Ok(_user) => match UserController::update(c, user.into_inner(), id) {
+                Ok(user) => Ok(json!(user)),
+                Err(e) => match e {
+                    NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
+                    _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+                },
+            },
+            Err(e) => match e {
+                NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
+                _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+            },
         },
     )
     .await
 }
 
 #[delete("/users/<id>")]
-async fn delete_user(db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
-    db.run(move |c| match UserController::delete(c, id) {
-        Ok(users) => Ok(json!(users)),
-        Err(e) => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-    })
-    .await
-}
-
-//LINKS
-//Not working rn, need to rework on auth 
-
-#[get("/links")]
-async fn all(auth: BasicAuth, db: DbConn) -> Result<Value, Custom<Value>> {
+async fn delete_user(auth: BasicAuth, db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
     db.run(
         move |c| match Verifier::verify(c, &auth.email, &auth.password) {
-            Ok(_link) => match LinkController::find_many(c) {
-                Ok(links) => Ok(json!(links)),
-                Err(e) => match e {
-                    NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
-                    _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-                },
-            },
-            Err(e) => match e {
-                NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
-                _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-            },
-        },
-    )
-    .await
-}
-
-#[get("/links/<id>")]
-async fn find_by_id(auth: BasicAuth, db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
-    db.run(
-        move |c| match Verifier::verify(c, &auth.email, &auth.password) {
-            Ok(_link) => match LinkController::find_one(c, id) {
-                Ok(links) => Ok(json!(links)),
-                Err(e) => match e {
-                    NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
-                    _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-                },
-            },
-            Err(e) => match e {
-                NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
-                _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-            },
-        },
-    )
-    .await
-}
-
-#[post("/", format = "json", data = "<new_link>")]
-async fn create_link(
-    auth: BasicAuth,
-    new_link: Json<NewLink>,
-    db: DbConn,
-) -> Result<Value, Custom<Value>> {
-    db.run(
-        move |c| match Verifier::verify(c, &auth.email, &auth.password) {
-            Ok(_link) => match LinkController::create(c, new_link.into_inner()) {
-                Ok(links) => Ok(json!(links)),
-                Err(e) => match e {
-                    NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
-                    _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-                },
-            },
-            Err(e) => match e {
-                NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
-                _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-            },
-        },
-    )
-    .await
-}
-
-#[put("/links/<id>", format = "json", data = "<link>")]
-async fn update_link(
-    auth: BasicAuth,
-    db: DbConn,
-    link: Json<Link>,
-    id: i32,
-) -> Result<Value, Custom<Value>> {
-    db.run(
-        move |c| match Verifier::verify(c, &auth.email, &auth.password) {
-            Ok(_link) => match LinkController::update(c, link.into_inner(), id) {
-                Ok(links) => Ok(json!(links)),
-                Err(e) => match e {
-                    NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
-                    _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-                },
-            },
-            Err(e) => match e {
-                NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
-                _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
-            },
-        },
-    )
-    .await
-}
-
-#[delete("/links/<id>")]
-async fn delete_link(auth: BasicAuth, db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
-    db.run(
-        move |c| match Verifier::verify(c, &auth.email, &auth.password) {
-            Ok(_link) => match LinkController::delete(c, id) {
-                Ok(links) => Ok(json!(links)),
+            Ok(_user) => match UserController::delete(c, id) {
+                Ok(user) => Ok(json!(user)),
                 Err(e) => match e {
                     NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
                     _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
@@ -208,58 +143,18 @@ fn bad_request() -> Value {
     json!("Bad request")
 }
 
-// #[rocket::main]
-// async fn main() {
-//     let _ = rocket::build()
-//         .mount(
-//             "/",
-//             routes![
-//                 all,
-//                 find_by_id,
-//                 create_link,
-//                 delete_link,
-//                 update_link,
-//                 user,
-//                 delete_user,
-//                 create_user,
-//                 update_user,
-//                 all_users,
-//             ],
-//         )
-//         .register(
-//             "/",
-//             catchers![not_found, unprocessable_entity, unauthorized, bad_request],
-//         )
-//         .attach(DbConn::fairing())
-//         .launch()
-//         .await;
-// }
-
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // let allowed_origins = AllowedOrigins::some_exact(&["https://www.acme.com"]);
-    // let allowed_origins = AllowedOrigins::all();
-
-    // You can also deserialize this
-    // let cors = rocket_cors::CorsOptions {
-    //     allowed_origins,
-    //     allowed_methods: vec![Method::Get, Method::Post, Method::Delete, Method::Put].into_iter().map(From::from).collect(),
-    //     allowed_headers: AllowedHeaders::some(&["Authorization", "Accept", "Access-Control-Allow-Origin"]),
-    //     allow_credentials: true,
-    //     ..Default::default()
-    // }
-    // .to_cors()?;
-
     let cors = rocket_cors::CorsOptions::default().to_cors()?;
     let _ = rocket::build()
         .mount(
             "/",
             routes![
-                all,
-                find_by_id,
-                create_link,
-                delete_link,
-                update_link,
+                // all,
+                // find_by_id,
+                // create_link,
+                // delete_link,
+                // update_link,
                 user,
                 delete_user,
                 create_user,
@@ -278,3 +173,114 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+//LINKS
+
+// #[get("/links")]
+// async fn all(auth: BasicAuth, db: DbConn) -> Result<Value, Custom<Value>> {
+//     db.run(
+//         move |c| match Verifier::verify(c, &auth.email, &auth.password) {
+//             Ok(_link) => match LinkController::find_many(c) {
+//                 Ok(links) => Ok(json!(links)),
+//                 Err(e) => match e {
+//                     NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
+//                     _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//                 },
+//             },
+//             Err(e) => match e {
+//                 NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
+//                 _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//             },
+//         },
+//     )
+//     .await
+// }
+
+// #[get("/links/<id>")]
+// async fn find_by_id(auth: BasicAuth, db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
+//     db.run(
+//         move |c| match Verifier::verify(c, &auth.email, &auth.password) {
+//             Ok(_link) => match LinkController::find_one(c, id) {
+//                 Ok(links) => Ok(json!(links)),
+//                 Err(e) => match e {
+//                     NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
+//                     _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//                 },
+//             },
+//             Err(e) => match e {
+//                 NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
+//                 _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//             },
+//         },
+//     )
+//     .await
+// }
+
+// #[post("/", format = "json", data = "<new_link>")]
+// async fn create_link(
+//     auth: BasicAuth,
+//     new_link: Json<NewLink>,
+//     db: DbConn,
+// ) -> Result<Value, Custom<Value>> {
+//     db.run(
+//         move |c| match Verifier::verify(c, &auth.email, &auth.password) {
+//             Ok(_link) => match LinkController::create(c, new_link.into_inner()) {
+//                 Ok(links) => Ok(json!(links)),
+//                 Err(e) => match e {
+//                     NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
+//                     _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//                 },
+//             },
+//             Err(e) => match e {
+//                 NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
+//                 _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//             },
+//         },
+//     )
+//     .await
+// }
+
+// #[put("/links/<id>", format = "json", data = "<link>")]
+// async fn update_link(
+//     auth: BasicAuth,
+//     db: DbConn,
+//     link: Json<Link>,
+//     id: i32,
+// ) -> Result<Value, Custom<Value>> {
+//     db.run(
+//         move |c| match Verifier::verify(c, &auth.email, &auth.password) {
+//             Ok(_link) => match LinkController::update(c, link.into_inner(), id) {
+//                 Ok(links) => Ok(json!(links)),
+//                 Err(e) => match e {
+//                     NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
+//                     _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//                 },
+//             },
+//             Err(e) => match e {
+//                 NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
+//                 _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//             },
+//         },
+//     )
+//     .await
+// }
+
+// #[delete("/links/<id>")]
+// async fn delete_link(auth: BasicAuth, db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
+//     db.run(
+//         move |c| match Verifier::verify(c, &auth.email, &auth.password) {
+//             Ok(_link) => match LinkController::delete(c, id) {
+//                 Ok(links) => Ok(json!(links)),
+//                 Err(e) => match e {
+//                     NotFound => Err(Custom(Status::NotFound, json!(e.to_string()))),
+//                     _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//                 },
+//             },
+//             Err(e) => match e {
+//                 NotFound => Err(Custom(Status::Unauthorized, json!("Unauthorized"))),
+//                 _ => Err(Custom(Status::InternalServerError, json!(e.to_string()))),
+//             },
+//         },
+//     )
+//     .await
+// }
